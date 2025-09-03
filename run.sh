c47838b706fc099933cc24ff28046e112537ba8a
Linnea Huusko <linnea.huusko@misu.su.se>
2025-09-03 15:47:49 +0200

    Initial commit, working version at the moment

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l
+#SBATCH -J neko_mixed_4_vreman
+#SBATCH -t 02:00:00
+#SBATCH --ntasks-per-node=8
+#SBATCH --nodes 1
+#SBATCH -p shared
+#SBATCH -A naiss2024-1-3
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+d="$(date +%F_%H-%M-%S)"
+srun -u -n 8 ./neko mixed.case > logfiles/logfile.log${d}
